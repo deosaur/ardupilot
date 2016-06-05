dd249ca7b2277a82c3d457cdca9c5d02fc0a8604
deosaur <saurabhdeo27@gmail.com>
2016-06-04 21:06:40 -0700

    Update UART_test.cpp

--- libraries/AP_HAL/examples/UART_test/UART_test.cpp
+++ libraries/AP_HAL/examples/UART_test/UART_test.cpp
@@ -97,6 +97,7 @@ void loop(void)
     ::printf("Hello on debug console at %.3f seconds\n", hal.scheduler->millis()*0.001f);
 #endif
 
+    //hal.console->printf()
     hal.scheduler->delay(1000);
 }
 
